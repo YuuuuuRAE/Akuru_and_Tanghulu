22db156b4d267b8deb6939392ba86f3a06b7e501
YuRae <jennyr0406@dgu.ac.kr>
2023-11-01 14:11:14 +0900

    Add Roulette Scene
    룰렛 씬 추가 (제작 중)

--- Akuru/Assets/Yurae/Scripts/Switch.cs
+++ Akuru/Assets/Yurae/Scripts/Switch.cs
@@ -8,8 +8,6 @@ public class Switch : MonoBehaviour
     public Image On;
     public Image Off;
 
-    public bool isbool = true;
-
 
     // Start is called before the first frame update
     void Start()
