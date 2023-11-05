e2f36e1cfe64ffb5bddd3a7a3cf0fd494aac27f7
nyyyyyr <127469346+YuuuuuRAE@users.noreply.github.com>
2023-11-06 03:03:28 +0900

    Update Roulette.cs

--- Akuru/Assets/Yurae/Scripts/Roulette/Roulette.cs
+++ Akuru/Assets/Yurae/Scripts/Roulette/Roulette.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Roulette : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform piecePrefab; // 룰렛에 표시되는 정보 프리팹
+    [SerializeField]
+    private Transform linePrefab; // 정보 구분 선 프리팹
+    [SerializeField]
+    private Transform pieceParent; //정보들이 배치되는 부모 Transform
+    [SerializeField]
+    private Transform lineParent; // 선들이 배치되는 부모 Transform
+    [SerializeField]
+    private RoulettePieceData[] roulettePieceData; // 룰렛에 표시되는 정보 배열
+
 
     // Update is called once per frame
     void Update()
