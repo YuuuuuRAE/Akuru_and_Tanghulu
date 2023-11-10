52a0f93e7815ed36d0f757b4d70503f13467e0ed
YuRae <jennyr0406@dgu.ac.kr>
2023-11-10 22:43:16 +0900

    Fix Option OnOff Butt
    옵션 버튼 수정

--- Akuru/Assets/Yurae/Scripts/Switch.cs
+++ Akuru/Assets/Yurae/Scripts/Switch.cs
@@ -8,6 +8,31 @@ public class Switch : MonoBehaviour
     public Image On;
     public Image Off;
 
+    [Header("0 = BGM, 1 = Sound, 2= Vibe")]
+    public int Option_num;
+
+    public GameObject OnButt;
+    public GameObject OffButt;
+
+    void Update()
+    {
+        switch (Option_num)
+        {
+            case 0:
+                OnButt.SetActive(GameManager.instance.isBGM);
+                OffButt.SetActive(!GameManager.instance.isBGM);
+                break;
+            case 1:
+                OnButt.SetActive(GameManager.instance.isSound);
+                OffButt.SetActive(!GameManager.instance.isSound);
+                break;
+            case 2:
+                OnButt.SetActive(GameManager.instance.isVibe);
+                OffButt.SetActive(!GameManager.instance.isVibe);
+                break;
+        }
+    }
+
 
     public void BGM()
     {
@@ -25,6 +50,7 @@ public class Switch : MonoBehaviour
         }
 
     }
+
 
     public void Sound()
     {
