441340e29ef7c61dca360c444af10aab06fbcba4
YuRae <jennyr0406@dgu.ac.kr>
2023-11-15 16:27:25 +0900

    Add LevelUp Alert
    레벨업 알럿 추가

---
+++ Akuru/Assets/Yurae/Scripts/Alert.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alert : MonoBehaviour
+{
+    [SerializeField]
+    private Text alertText; //알럿 텍스트
+    [SerializeField]
+    private int currentLV; // 현재 레벨 게임 매니저의 레벨과 비교하여 알럿을 띄우고 텍스트를 변환하기 위함
+
+    [SerializeField]
+    private float AlertTime; //알럿을 띄울 시간
+    private float currentTime; //현재 시간
+
+    [SerializeField]
+    private GameObject alertUI;
+    [SerializeField]
+    private int UIHeight; //UI 높이
+
+    private void Update()
+    {
+        if (GameManager.instance.CurrentLevel > currentLV)
+        {
+            alertUI.SetActive(true);
+            if(GameManager.instance.CurrentLevel <= 5)
+            alertText.text = "레벨 " + GameManager.instance.CurrentLevel.ToString() + " 달성!\n" + "이제 나무에서 " + FruitName(GameManager.instance.CurrentLevel) + " 자라요!";
+            else
+            {
+                alertUI.GetComponent<RectTransform>().sizeDelta = new Vector2(alertUI.GetComponent<RectTransform>().sizeDelta.x, UIHeight);
+                alertText.text = "레벨 " + GameManager.instance.CurrentLevel.ToString() + "달성!";
+            }
+            currentTime += Time.deltaTime;
+
+            if(currentTime >= AlertTime)
+            {
+                alertUI.SetActive(false);
+                currentLV = GameManager.instance.CurrentLevel;
+                currentTime = 0;
+            }
+        }
+    }
+
+    public string FruitName(int F_Num)
+    {
+        switch (F_Num)
+        {
+            case 2:
+                return "청포도가";
+            case 3:
+                return "귤이";
+            case 4:
+                return "파인애플이";
+            case 5:
+                return "블루베리가";
+            default:
+                return "";
+
+        }
+
+    }
+}
